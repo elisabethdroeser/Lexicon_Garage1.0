2594f690abf9f4b1bb7e3f205ebbc32dbc2e94e0
elisabeth.droeser@gmail.com <elisabeth.droeser@gmail.com>
2021-12-23 14:46:39 +0100

    Work with garage.

--- Lexicon_Garage1.0/Garage.cs
+++ Lexicon_Garage1.0/Garage.cs
@@ -11,16 +11,21 @@ namespace LexiconGarage1
     //Garaget: En representation av själva byggnaden. Garaget är en plats där en mängd av fordon kan förvaras.Garaget kan alltså representeras som en samling av fordon.
     //generisk samling av fordon, begränsas av en constraint. 
 
-    internal class Garage //: IEnumerable<T> where T : IVehicle
+    //internal class Garage //: IEnumerable<T> where T : IVehicle
+    public class Garage : IEnumerable
     {
         public Garage(int size)
         {
-            vehicles = new Vehicle[size]; 
+            vehicles = new Vehicle[size];
         }
 
-         
+
         private Vehicle[] vehicles;
 
+        // public Garagee[] vehicles)
+        //    {
+        //
+        //    }
         //public IEnumerator<Vehicle> GetEnumerator()
         //{
         //    throw new NotImplementedException();
