efedff23fb2b0c0cfbecf674db0896610f0cc4a1
elisabeth.droeser@gmail.com <elisabeth.droeser@gmail.com>
2021-12-27 17:12:21 +0100

    Update menu with method alternatives.

--- Lexicon_Garage1.0/Program.cs
+++ Lexicon_Garage1.0/Program.cs
@@ -2,9 +2,10 @@
 using LexiconGarage.Vehicles;
 using System.Collections.Generic;
 
-//var garageManager = new GarageManager();
-//garageManager.Run();
+var garageManager = new GarageManager();
+garageManager.Menu();
 //garageManager.PrintAllVehicles();
+
 
 var gh = new GarageHandler(5);
 gh.SeedData();
---
+++ Lexicon_Garage1.0/UserInterface/UI.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LexiconGarage.Vehicles;
+
+namespace LexiconGarage1;
+
+public class UI
+{ 
+
+}
